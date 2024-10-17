3916dd24863c4201f0052ecbe85654810424000e
HaneenAhmedMohy <haneenahmedmohy@gmail.com>
2024-10-17 11:23:03 +0300

    fix

--- lib/src/widgets/sticker_widget/sticker_box.dart
+++ lib/src/widgets/sticker_widget/sticker_box.dart
@@ -3,53 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:sticker_editor_plus/src/model/picture_model.dart';
 
 class StickerEditingBox extends StatefulWidget {
-  /// Your widget should be move within this [boundWidth]
   final double boundWidth;
-
-  /// Your widget should be move within this [boundHeight]
   final double boundHeight;
-
-  /// This picture model where you pass necessary fields
   final PictureModel pictureModel;
-
-  /// If you use onCancel then you Have to manage IsSelected field in PicturModel
   final Function()? onCancel;
-
-  /// If you use onTap then you Have to manage IsSelected field in PicturModel
   final Function()? onTap;
-
   final bool viewOnly;
-
-  /// Custom controller Icons
   final Icon? resizeIcon;
   final Icon? rotateIcon;
   final Icon? closeIcon;
 
-  /// Create a [StickerEditingBox] widget
-  ///
-  /// [pictureModel] detail of your picture
-  /// [onTap] callback function that called when you tap on [StickerEditingBox]
-  /// [onCancel] callback function that called when you tap on Cross icon in [StickerEditingBox] border
-  const StickerEditingBox(
-      {Key? key,
-      required this.boundWidth,
-      required this.boundHeight,
-      required this.pictureModel,
-      this.viewOnly = false,
-      this.resizeIcon,
-      this.rotateIcon,
-      this.closeIcon,
-      this.onTap,
-      this.onCancel})
-      : super(key: key);
+  const StickerEditingBox({
+    Key? key,
+    required this.boundWidth,
+    required this.boundHeight,
+    required this.pictureModel,
+    this.viewOnly = false,
+    this.resizeIcon,
+    this.rotateIcon,
+    this.closeIcon,
+    this.onTap,
+    this.onCancel,
+  }) : super(key: key);
 
   @override
   _StickerEditingBoxState createState() => _StickerEditingBoxState();
 }
 
 class _StickerEditingBoxState extends State<StickerEditingBox> {
-  Offset deltaOffset = const Offset(0, 0);
-
   double? lastScale;
 
   @override
@@ -70,11 +51,8 @@ class _StickerEditingBoxState extends State<StickerEditingBox> {
           child: GestureDetector(
             onScaleStart: (_) => lastScale = widget.pictureModel.scale,
             onScaleUpdate: (tap) {
-              if (widget.viewOnly) {
-                return;
-              }
+              if (widget.viewOnly) return;
 
-              // var intialScale = tap.scale;
               setState(() {
                 if (tap.pointerCount == 2) {
                   // Handle rotation
@@ -86,26 +64,28 @@ class _StickerEditingBoxState extends State<StickerEditingBox> {
                   } else {
                     widget.pictureModel.scale += 0.04;
                   }
+                } else {
+                  // Adjust movement speed based on scale
+                  final movementFactor = 1 / widget.pictureModel.scale;
+                  
+                  // Calculate new position with scale compensation
+                  final newLeft = (widget.pictureModel.left + (tap.focalPointDelta.dx * movementFactor))
+                      .clamp(0.0, widget.boundWidth - (50 * widget.pictureModel.scale));
+                  
+                  final newTop = (widget.pictureModel.top + (tap.focalPointDelta.dy * movementFactor))
+                      .clamp(0.0, widget.boundHeight - (50 * widget.pictureModel.scale));
+
+                  // Update position
+                  widget.pictureModel.left = newLeft;
+                  widget.pictureModel.top = newTop;
                 }
-
-                // Use the focal point delta directly for dragging, without scaling it down
-                final newLeft = (widget.pictureModel.left + tap.focalPointDelta.dx).clamp(0.0, widget.boundWidth - 50);
-                final newTop = (widget.pictureModel.top + tap.focalPointDelta.dy).clamp(0.0, widget.boundHeight - 50);
-
-                // Update position
-                widget.pictureModel.left = newLeft;
-                widget.pictureModel.top = newTop;
               });
 
               lastScale = tap.scale;
             },
             onTap: () {
               if (widget.onTap == null) {
-                if (widget.pictureModel.isSelected) {
-                  setState(() => widget.pictureModel.isSelected = false);
-                } else {
-                  setState(() => widget.pictureModel.isSelected = true);
-                }
+                setState(() => widget.pictureModel.isSelected = !widget.pictureModel.isSelected);
               } else {
                 widget.onTap!();
               }
@@ -122,66 +102,71 @@ class _StickerEditingBoxState extends State<StickerEditingBox> {
                         : Image.asset(widget.pictureModel.stringUrl, height: 50, width: 50),
                   ),
                 ),
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  child: GestureDetector(
-                    onPanUpdate: (tap) {
-                      if (!tap.delta.dx.isNegative) {
-                        setState(() => widget.pictureModel.angle -= 0.05);
-                      } else {
-                        setState(() => widget.pictureModel.angle += 0.05);
-                      }
-                    },
-                    child: widget.pictureModel.isSelected
-                        ? Container(
-                            padding: const EdgeInsets.all(2),
-                            decoration: BoxDecoration(
-                                border: Border.all(color: Colors.black, width: 1), shape: BoxShape.circle, color: Colors.white),
-                            child: widget.rotateIcon ?? const Icon(Icons.sync_alt, color: Colors.black, size: 12),
-                          )
-                        : Container(),
-                  ),
-                ),
-                Positioned(
-                  top: 3,
-                  right: 3,
-                  child: InkWell(
-                    onTap: () {
-                      if (widget.onCancel != null) {
-                        widget.onCancel!();
-                      }
-                      setState(() => widget.pictureModel.isSelected = false);
-                    },
-                    child: widget.pictureModel.isSelected
-                        ? Container(
-                            padding: const EdgeInsets.all(2),
-                            decoration: BoxDecoration(
-                                border: Border.all(color: Colors.black, width: 1), shape: BoxShape.circle, color: Colors.white),
-                            child: widget.closeIcon ?? const Icon(Icons.close, color: Colors.black, size: 12),
-                          )
-                        : Container(),
-                  ),
-                ),
-                Positioned(
-                  bottom: 3,
-                  right: 3,
-                  child: GestureDetector(
+                if (widget.pictureModel.isSelected) ...[
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    child: GestureDetector(
                       onPanUpdate: (tap) {
-                        if (tap.delta.dx.isNegative && widget.pictureModel.scale > .5) {
-                          setState(() => widget.pictureModel.scale -= 0.05);
-                        } else if (!tap.delta.dx.isNegative && widget.pictureModel.scale < 5) {
-                          setState(() => widget.pictureModel.scale += 0.05);
-                        }
+                        setState(() {
+                          widget.pictureModel.angle += tap.delta.dx.isNegative ? 0.05 : -0.05;
+                        });
                       },
-                      child: widget.pictureModel.isSelected
-                          ? Container(
-                              padding: const EdgeInsets.all(2),
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.black, width: 1), color: Colors.white, shape: BoxShape.circle),
-                              child: widget.resizeIcon ?? const Icon(Icons.crop, color: Colors.black, size: 12))
-                          : Container()),
-                ),
+                      child: Container(
+                        padding: const EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1),
+                          shape: BoxShape.circle,
+                          color: Colors.white
+                        ),
+                        child: widget.rotateIcon ?? const Icon(Icons.sync_alt, color: Colors.black, size: 12),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 3,
+                    right: 3,
+                    child: InkWell(
+                      onTap: () {
+                        if (widget.onCancel != null) widget.onCancel!();
+                        setState(() => widget.pictureModel.isSelected = false);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1),
+                          shape: BoxShape.circle,
+                          color: Colors.white
+                        ),
+                        child: widget.closeIcon ?? const Icon(Icons.close, color: Colors.black, size: 12),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 3,
+                    right: 3,
+                    child: GestureDetector(
+                      onPanUpdate: (tap) {
+                        setState(() {
+                          if (tap.delta.dx.isNegative && widget.pictureModel.scale > .5) {
+                            widget.pictureModel.scale -= 0.05;
+                          } else if (!tap.delta.dx.isNegative && widget.pictureModel.scale < 5) {
+                            widget.pictureModel.scale += 0.05;
+                          }
+                        });
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 1),
+                          color: Colors.white,
+                          shape: BoxShape.circle
+                        ),
+                        child: widget.resizeIcon ?? const Icon(Icons.crop, color: Colors.black, size: 12),
+                      ),
+                    ),
+                  ),
+                ],
               ],
             ),
           ),
